428c5b60a6d14209eb91bc5bec23b7c22a1f57be
kostDev <kostyaboy777@gmail.com>
2025-05-26 08:10:44 +0300

    change: main

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,19 @@
-// mod vfs;
 mod cli;
 mod ipc;
 mod proto;
-// mod vfsd;
-// mod proto;
-// mod ipc;
 
 use crate::cli::handle_cli;
-// use crate::vfs::VirtualFileSystem;
-
 // static WORK_FILE: &str = "./fs/data.vfs";
 
-// Пример використання
 fn main() {
     // let mut fs = VirtualFileSystem::new();
     // fs.add_file("hello.txt", "Привіт, світ!".as_bytes());
-    // fs.add_file("log.txt", "Це лог файл".as_bytes());
-
     // fs.log_list_info();
-    //
     // fs.show_file("hello.txt");
-    handle_cli();
     // fs.log_list_info();
     // fs.show_file("test.txt");
     // fs.save_to_file(WORK_FILE).unwrap();
     // fs.load_from_file(WORK_FILE).unwrap();
     // fs.log_list_info();
+    handle_cli();
 }
